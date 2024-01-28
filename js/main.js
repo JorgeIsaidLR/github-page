21e456e1bb1edc2c3bf70a79c561d87829f7eb77
JorgeIsaidLR <155588531+JorgeIsaidLR@users.noreply.github.com>
2024-01-27 23:23:07 -0600

    Update main.js
    Primer prueba obtener solo pais

--- js/main.js
+++ js/main.js
@@ -1,38 +1,21 @@
-//alert("Hola mundo");
-// Hacer una petición para un usuario con ID especifico
-
-
-let ipCliente = document.getElementById('ipCliente');
-let country_name=document.getElementById('country_name');
-let continent_name=document.getElementById('continent_name');
-let current_time=document.getElementById('current_time');
-
+let country_name = document.getElementById('country_name');
 
 const solicitudAPI = () => {
-  // Coloca aquí el código que deseas ejecutar cuando la página se actualice
-  console.log("La página se ha actualizado. Tu código aquí.");
+    // Realizar solicitud al servidor para obtener el país
+    axios.get('https://itp-bdd-jilr-01.000webhostapp.com/php-geoip-api/get-country.php')
+        .then(function (response) {
+            // Manejar respuesta exitosa
+            console.log(response.data);
+            country_name.innerHTML = response.data.country_name;
+        })
+        .catch(function (error) {
+            // Manejar error
+            console.log(error);
+        });
+};
 
-
-axios.get('https://itp-bdd-jilr-01.000webhostapp.com/php-geoip-api/index.php')
-  .then(function (response) {
-    // manejar respuesta exitosa
-    console.log(response.data);
-    ipCliente.innerHTML = response.data.ipCliente;
-    country_name.innerHTML = response.data.country_name;
-    continent_name.innerHTML = response.data.continent_name;
-    current_time.innerHTML = response.data.current_time;
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .finally(function () {
-    // siempre sera executado
-  });
-
-
-
-
+// Llamar a la función al cargar la página
+window.addEventListener('load', solicitudAPI);
 
 
 
