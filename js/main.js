57dff6e690edff9ac1853365ce73e8cc214bbb2d
JorgeIsaidLR <155588531+JorgeIsaidLR@users.noreply.github.com>
2024-01-27 23:46:31 -0600

    Update main.js
    recomunicacion test

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
-let country_name = document.getElementById('country_name');
+//alert("Hola mundo");
+// Hacer una petición para un usuario con ID especifico
+
+
+let ipCliente = document.getElementById('ipCliente');
+let country_name=document.getElementById('country_name');
+let continent_name=document.getElementById('continent_name');
+let current_time=document.getElementById('current_time');
+
 
 const solicitudAPI = () => {
-    // Realizar solicitud al servidor para obtener solo el país
-    axios.get('https://itp-bdd-jilr-01.000webhostapp.com/php-geoip-api/get-country.php')
-        .then(function (response) {
-            // Manejar respuesta exitosa
-            console.log(response.data);
-            country_name.innerHTML = response.data.country_name;
-        })
-        .catch(function (error) {
-            // Manejar error
-            console.log(error);
-        });
+  // Coloca aquí el código que deseas ejecutar cuando la página se actualice
+  console.log("La página se ha actualizado. Tu código aquí.");
+
+
+axios.get('https://itp-bdd-jilr-01.000webhostapp.com/php-geoip-api/index.php')
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response.data);
+    ipCliente.innerHTML = response.data.ipCliente;
+    country_name.innerHTML = response.data.country_name;
+    continent_name.innerHTML = response.data.continent_name;
+    current_time.innerHTML = response.data.current_time;
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  });
+
+
+
+
+
+
+
 };
 
-// Llamar a la función al cargar la página
+// llama al evento load cada vez que se actulaiza la pagina
+// llama a la funcion solicitud que tiene la rutina de llamas a la api desde axios
 window.addEventListener('load', solicitudAPI);
